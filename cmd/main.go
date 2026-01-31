e050915cf91a8637f30f995ba6e3c2dc062b3161
Azamov Samandar <admin@jscorp.uz>
2026-01-31 12:45:27 +0500

    feat(redis): redist qo'shildi

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/JscorpTech/websocket/internal/ws"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/redis/go-redis/v9"
 )
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true // productionda domain tekshiriladi
+		return true
 	},
 }
 
@@ -43,15 +50,46 @@ func echo(r *http.Request, w http.ResponseWriter, hub *ws.Hub) {
 }
 
 func main() {
-	s := gin.Default()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	router := gin.Default()
 	hub := ws.NewHub()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		fmt.Println("Regisga ulanishda xatolik yuz berdi")
+		return
+	}
+
 	go hub.Run()
 
-	s.GET("/ws", func(c *gin.Context) {
+	router.GET("/ws", func(c *gin.Context) {
 		echo(c.Request, c.Writer, hub)
 	})
-	s.GET("/health", func(c *gin.Context) {
+	router.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
-	s.Run()
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		fmt.Println("server ishga tushdi :8080")
+		if err := srv.ListenAndServe(); err != nil {
+			fmt.Println("xatolik yuz berdi")
+		}
+	}()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+	ctx, cancel = context.WithTimeout(ctx, 5*time.Second)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Serverni to'xtatishda xatolik yuz berdi")
+	}
+	fmt.Println("Server to'xtatildi")
 }
